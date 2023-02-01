01aa3445d493ee1dd2c3ccdcb7f7b0177cc6876e
ouhaibo1980 <105035626+ouhaibo1980@users.noreply.github.com>
2023-02-01 13:21:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="tv.nxog.top";  //你需要反代的域名
+        url.hostname="m.nxog.top";  //你需要反代的域名
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
